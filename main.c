cd8abb2ece16c2b66e1786a97c4725740c8ee149
ImperialEQZ <alexanders.borchenko@gmail.com>
2024-05-14 17:17:33 +0300

    Я полностью понял тему, это качественный комит

--- main.c
+++ main.c
@@ -5,43 +5,48 @@
 #include "matrix.c"
 #include "vectorVoid.h"
 
-#define ASSERT_FILES(filename1, filename2) assertTXT(filename1, filename2, __FUNCTION__)
-#define MAX_FILE_SIZE 1024
+FILE* check_file_open_without_error(const char* filename) {
+    FILE *file = fopen(filename, "r");
 
-void assertTXT(const char *filename1, const char *filename2, char const *funcName)
-{
-    FILE *f1 = fopen(filename1, "r");
-    FILE *f2 = fopen(filename2, "r");
-    if (f1 == NULL || f2 == NULL) {
-        printf("Error\n");
-        return;
-    }
-    char buffer1[MAX_FILE_SIZE];
-    char buffer2[MAX_FILE_SIZE];
-
-    while (fgets(buffer1, MAX_FILE_SIZE, f1) != NULL
-           && fgets(buffer2, MAX_FILE_SIZE, f2) != NULL) {
-        if (strcmp(buffer1, buffer2) != 0) {
-            printf("Error: files is not similar\n");
-            fclose(f1);
-            fclose(f2);
-            return;
-        }
-    }
-    printf("Testing %s - %s passed\n", funcName);
-}
-
-int task_1(const char* filename) {
-    FILE* file = fopen(filename, "r");
     if (file == NULL) {
         printf("error reading\n");
         exit(1);
     }
 
-    long long n;
+    return file;
+}
+
+void generate_random_matrix_file(const char *filename, size_t n) {
+    FILE* file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    fprintf(file, "%lld\n", n);
+
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
+            fprintf(file, "%d ", rand() % 10);
+        }
+
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+}
+
+void transpose_matrix_in_file(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    long long int n;
     fscanf(file, "%lld", &n);
 
-    matrix matrix = getMemMatrix((int) n, (int) n);
+    matrix matrix = getMemMatrix((int) n , (int) n);
 
     for (size_t i = 0; i < n; i++)
         for (size_t j = 0; j < n; j++)
@@ -101,16 +106,9 @@ void task_2(const char* filename) {
     deleteVectorV(&v);
     fclose(file);
 }
-void test_task_1() {
-    const char *filename_1 = "task_1.txt";
-    const char *exp_file_1 = "task_1_test.txt";
-    int ans = task_1(filename_1);
-    if (ans == 0)
-        ASSERT_FILES(filename_1, exp_file_1);
-}
 
 void test_task_2_zero_numb() {
-    const char filename[] = "task_2.txt";
+    const char filename[] = "19_2_test_zero_numb";
 
     FILE* file = fopen(filename, "w");
     fclose(file);
@@ -128,7 +126,7 @@ void test_task_2_zero_numb() {
 }
 
 void test_task_2_three_numb() {
-    const char filename[] = "task_2_test_2.txt";
+    const char filename[] = "19_2_test_three_numb";
 
     float num_1 = 3.143423;
     float num_2 = 2.241518;
@@ -159,7 +157,22 @@ void test_task_2() {
 }
 
 int main() {
-    test_task_1();
+    generate_random_matrix_file("ex", 5);
+    FILE *file = check_file_open_without_error("ex");
+
+
+    long long n;
+    fscanf(file, "%lld", &n);
+
+    matrix matrix = getMemMatrix((int) n, (int) n);
+
+    for (size_t i = 0; i < n; i++)
+        for (size_t j = 0; j < n; j++)
+            fscanf(file, "%d", &matrix.values[i][j]);
+
+    outputMatrix(matrix);
+
+    fclose(file);
     test_task_2();
     return 0;
 }
